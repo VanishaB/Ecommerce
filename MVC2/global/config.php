afaa0578940fda1324aa8742585a96e58b0f0b48
julien graziani <juliengraziani@hotmail.fr>
2018-07-10 13:08:12 +0200

    Correction fichiers

--- MVC2/global/config.php
+++ MVC2/global/config.php
@@ -1,7 +1,7 @@
 <?php
 
 //	Identifiants pour la bdd. need pour pdo2.php
-define('SQL_DSN', 		'mysql:dbname=Users;host=localhost');
+define('SQL_DSN', 		'mysql:dbname=site;host=localhost');
 define('SQL_USERNAME', 'root');
 define('SQL_PASSWORD', '');
 
